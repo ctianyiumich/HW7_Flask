2719803b771d4b59bfaf13e0d38f259f6322e8ce
ctianyiumich <71205401+ctianyiumich@users.noreply.github.com>
2021-04-20 07:47:19 -0400

    Update top_headlines.py

--- top_headlines.py
+++ top_headlines.py
@@ -1,1 +1,24 @@
+from flask import Flask, render_template
+import json
+import secrets
+import requests
+api_url= f"https://api.nytimes.com/svc/topstories/v2/technology.json?api-key={secrets.api_key}"
+hl_list = []
+response = requests.get(api_url)
+nytimes_dict = json.loads(response.text)
+for result in nytimes_dict['results'][:5]:
+    hl_list.append(result)
 
+app = Flask(__name__)
+
+@app.route('/')
+def index():     
+    return '<h1>Welcome!</h1>'
+
+@app.route('/name/Buffy')
+def name():
+    return render_template('name.html', name = "Buffy", headlines_list = hl_list)
+
+if __name__ == '__main__':
+
+    app.run(debug=True)
